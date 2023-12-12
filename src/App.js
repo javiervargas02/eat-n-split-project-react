5f6d6a48501ba876b4b3f8458995da0968fe173b
javiervargas02 <javier.v.021204@gmail.com>
2023-12-11 20:46:47 -0600

    Add custom components and add logic for conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
+import FriendsList from "./components/FriendsList";
+import FormAddFriend from "./components/FormAddFriend";
+import Button from "./components/Button";
+import FormSplitBill from "./components/FormSplitBill";
+import { useState } from "react";
 
 export default function App() {
-  console.log(initialFriends);
+  const [isOpenAddFriend, setIsOpenAddFriend] = useState(false);
   return (
-    <div>
-      
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        {isOpenAddFriend && <FormAddFriend />}
+        {/* Learned that I cannot use onClick on a custom component, but instead, I have to pass it as a prop to the component and the use it on the html */}
+        <Button onClick={() => setIsOpenAddFriend((open) => !open)}>
+          {isOpenAddFriend ? "Close" : "Add friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
     </div>
   );
 }
