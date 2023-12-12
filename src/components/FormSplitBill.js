38426b54fd2a8501e8898dc050ba20b2e03e555a
javiervargas02 <javier.v.021204@gmail.com>
2023-12-11 20:42:21 -0600

    Add main component to split the bill

---
+++ src/components/FormSplitBill.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "./Button";
+
+export default function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💵 Bill value</label>
+      <input type="text" />
+
+      <label>🙋🏻‍♂️ Your expense</label>
+      <input type="text" />
+
+      <label>👬 X's expense</label>
+      <input type="text" disabled/>
+
+      <label>🤑 Who is paying the bill?</label>
+      <select >
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
