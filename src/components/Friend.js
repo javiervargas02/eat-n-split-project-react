cc32f5cdd5b54ef26ccf8b4786a4411c3750d04c
javiervargas02 <javier.v.021204@gmail.com>
2023-12-11 20:22:24 -0600

    Change how component displays information

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,5 +1,23 @@
 import React from "react";
+import Button from "./Button";
 
 export default function Friend({ friend }) {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You are even with {friend.name}</p>}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      )}
+      <Button>Select</Button>
+    </li>
+  );
 }
