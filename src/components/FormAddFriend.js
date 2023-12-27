646b500dd1c73b171c61278d71e50c680673715c
javiervargas02 <javier.v.021204@gmail.com>
2023-12-27 17:57:43 -0600

    Lift up state and add logic
    Lift up initial friends state becuase that state is shared with the friends list component. Implement handleSubmit function to add a new friend to the friends array. Convert normal input fields to controlled input fields.

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -1,13 +1,42 @@
+import { useState } from "react";
 import Button from "./Button";
 
-export default function FormAddFriend() {
+export default function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const friendId = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `${image}?u=${friendId}`,
+      balance: 0,
+      id: friendId,
+    };
+    onAddFriend((previousFriends) => [...previousFriends, newFriend]);
+    console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend"> 
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👯‍♂️ Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={({ target }) => setName(target.value)}
+      />
 
       <label>📸 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={({ target }) => setImage(target.value)}
+      />
 
       <Button>Add</Button>
     </form>
