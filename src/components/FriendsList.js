d7706abff5608b2fae47e08f138e2bc80b0e88d8
javiervargas02 <javier.v.021204@gmail.com>
2023-12-27 17:59:43 -0600

    Lift up state and add props
    Lift up friends state because that state is shared with the form add friend component. Also use this component to drill state to the friend component

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,32 +1,20 @@
 import React from "react";
 import Friend from "./Friend";
 
-const friends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
-export default function FriendsList() {
+export default function FriendsList({
+  friends,
+  onSelectFriend,
+  selectedFriend,
+}) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id}/>
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelectFriend={onSelectFriend}
+          selectedFriend={selectedFriend}
+        />
       ))}
     </ul>
   );
