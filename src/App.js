0212bd03ec1e21a49ef61bc9ac7856cf4c3825bf
javiervargas02 <javier.v.021204@gmail.com>
2023-12-27 17:55:36 -0600

    Add state and logic
    Add initial friends to render, implement handleSplitBill function as the main functionality of the project, add state for components to share, modify and retrieve data

--- src/App.js
+++ src/App.js
@@ -4,19 +4,65 @@ import Button from "./components/Button";
 import FormSplitBill from "./components/FormSplitBill";
 import { useState } from "react";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
   const [isOpenAddFriend, setIsOpenAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    // close the bill form and reset state
+    setSelectedFriend(null);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {isOpenAddFriend && <FormAddFriend />}
+        <FriendsList
+          friends={friends}
+          onSelectFriend={setSelectedFriend}
+          selectedFriend={selectedFriend}
+        />
+        {isOpenAddFriend && <FormAddFriend onAddFriend={setFriends} />}
         {/* Learned that I cannot use onClick on a custom component, but instead, I have to pass it as a prop to the component and the use it on the html */}
         <Button onClick={() => setIsOpenAddFriend((open) => !open)}>
           {isOpenAddFriend ? "Close" : "Add friend"}
         </Button>
       </div>
-      <FormSplitBill />
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
